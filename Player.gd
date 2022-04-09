fbb21aec08baaf0330040ab9527603bf27ce7666
Maciej Piatek <magic@2fd5.com>
2022-04-09 14:11:53 +0100

    new: add init Player script
    * 5. Scripting the Player - Part 1

---
+++ Player.gd
@@ -0,0 +1,9 @@
+extends KinematicBody2D
+
+var score : int = 0
+var speed : int = 200
+var jumpForce : int = 600
+var gravity : int = 800
+var velocity : Vector2 = Vector2()
+onready var sprite : Sprite = $Sprite
+
